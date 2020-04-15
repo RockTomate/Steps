fb81a890ad16f9bf23920a5c803078978869ff60
Elmar Talibzade <elmar.talibzade@gmail.com>
2020-04-15 01:35:51 +0100

    Added a "Print List" step

---
+++ Classes/General/PrintListStep.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using HardCodeLab.RockTomate.Core.Data;
+using HardCodeLab.RockTomate.Core.Steps;
+using HardCodeLab.RockTomate.Core.Enums;
+using HardCodeLab.RockTomate.Core.Logging;
+using HardCodeLab.RockTomate.Core.Attributes;
+using HardCodeLab.RockTomate.Core.Extensions;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Print List", "Prints contents of a list, array and anything that implements \"IEnumerable\".")]
+    public class PrintListStep : Step
+    {
+        /// <summary>
+        /// The enumerable through which this will iterate through
+        /// </summary>
+        [NonSerialized]
+        [InputField(tooltip: "Item list which contents will be printed out.", fieldMode: FieldMode.FormulaOnly, required: true)]
+        public IEnumerable ItemList;
+
+        [InputField("Type", "Type of message which will be logged.")]
+        public LogTier MessageType = LogTier.Debug;
+
+        [InputField(tooltip: "If true, message will be printed into Unity's Console window")]
+        public bool PrintUnityConsole = true;
+
+        [InputField(tooltip: "If true, the logs will be saved immediately into a log file (along with pending logs) after this message has been printed out.")]
+        public bool FlushImmediately = false;
+
+        /// <inheritdoc />
+        protected override IEnumerator OnExecute(JobContext context)
+        {
+            if (ItemList.IsEmpty())
+                yield break;
+
+            foreach (var item in ItemList)
+            {
+                Print(item);
+                yield return null;
+            }
+
+            IsSuccess = true;
+        }
+
+        private void Print(object item)
+        {
+            var message = item.ToString();
+            RockLog.WriteLine(MessageType, message);
+
+            if (FlushImmediately)
+                RockLog.FlushLogs();
+
+            if (!PrintUnityConsole)
+                return;
+
+            switch (MessageType)
+            {
+                case LogTier.Debug:
+
+                    Debug.Log(message);
+
+                    break;
+
+                case LogTier.Warning:
+
+                    Debug.LogWarning(message);
+
+                    break;
+
+                case LogTier.Error:
+
+                    Debug.LogError(message);
+
+                    break;
+            }
+        }
+    }
+}
