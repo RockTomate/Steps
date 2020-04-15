386a4c21443e934f181ff980c1c591e36e43981f
Elmar Talibzade <elmar.talibzade@gmail.com>
2020-04-15 22:34:08 +0100

    Added new steps

---
+++ Classes/Assets/CopyAssetStep.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEditor;
+using HardCodeLab.RockTomate.Core.Steps;
+using HardCodeLab.RockTomate.Core.Helpers;
+using HardCodeLab.RockTomate.Core.Attributes;
+using UnityEngine;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Copy Asset", "Makes a copy of an Asset file", StepCategories.AssetsCategory)]
+    public class CopyAssetStep : SimpleStep
+    {
+        [InputField(tooltip: "Path to a file that will be duplicated.", required: true)]
+        public string AssetPath;
+
+        [InputField(tooltip: "Path of a duplicated asset.", required: true)]
+        public string NewAssetPath;
+
+        /// <inheritdoc />
+        protected override bool OnStepStart()
+        {
+            return AssetDatabase.CopyAsset(PathHelpers.ConvertToAssetsPath(AssetPath), PathHelpers.ConvertToAssetsPath(NewAssetPath));
+        }
+    }
+}
