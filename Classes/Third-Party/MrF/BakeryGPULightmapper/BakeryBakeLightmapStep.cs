13fef8d7977c112ef14a49fcc10b3373403621c5
Elmar Talibzade <elmar.talibzade@gmail.com>
2020-04-27 00:24:43 +0100

    Added Bakey GPU Lightmapper step

---
+++ Classes/Third-Party/MrF/BakeryGPULightmapper/BakeryBakeLightmapStep.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using HardCodeLab.RockTomate.Core.Data;
+using HardCodeLab.RockTomate.Core.Steps;
+using HardCodeLab.RockTomate.Core.Attributes;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Bakery - Bake Lightmap", "Bakes a lightmap using GPU", "\"Bakery - GPU Lightmapper\" Plugin")]
+    public class BakeryBakeLightmapStep : Step
+    {
+        /// <inheritdoc />
+        protected override IEnumerator OnExecute(JobContext context)
+        {
+            var bakery = ftRenderLightmap.instance != null
+                ? ftRenderLightmap.instance
+                : ScriptableObject.CreateInstance<ftRenderLightmap>();
+
+            bakery.LoadRenderSettings();
+            bakery.RenderButton(false);
+            while (ftRenderLightmap.bakeInProgress)
+                yield return null;
+            
+            bakery.Close();
+            IsSuccess = true;
+        }
+
+        /// <inheritdoc />
+        public override string InProgressText
+        {
+            get { return "Baking..."; }
+        }
+    }
+}
