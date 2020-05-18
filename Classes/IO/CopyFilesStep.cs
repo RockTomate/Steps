890b0f4b6507eb56addc6d755174de1871cfc8af
Elmar Talibzade <elmar.talibzade@gmail.com>
2020-05-18 17:11:58 +0100

    [WIP] Added new steps

---
+++ Classes/IO/CopyFilesStep.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using HardCodeLab.RockTomate.Core.Steps;
+using HardCodeLab.RockTomate.Core.Logging;
+using HardCodeLab.RockTomate.Core.Attributes;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Copy Files", "Copies the files to another destination", StepCategories.FileSystemCategory)]
+    public class CopyFilesStep : SimpleStep
+    {
+        [InputField(tooltip: "Path of the files that will be copied.", required: true)]
+        public string[] Paths;
+
+        [InputField(tooltip: "Path of the files that will be copied.", required: true)]
+        public string BaseDirectory;
+
+        [InputField(tooltip: "Path of the files that will be copied.", required: true)]
+        public string DestinationDirectory;
+
+        [InputField(tooltip: "If enabled, the destination copy will be overwritten if it already exists.")]
+        public bool Overwrite = false;
+
+        /// <inheritdoc />
+        protected override bool OnStepStart()
+        {
+            try
+            {
+                foreach (var path in Paths)
+                {
+                    var destination = path.Replace(BaseDirectory, DestinationDirectory);
+
+                    if (Overwrite && File.Exists(destination))
+                    {
+                        File.Delete(destination);
+                    }
+
+                    File.Copy(path, destination, Overwrite);
+                }
+            }
+            catch (Exception ex)
+            {
+                RockLog.LogException(this, ex);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Classes/IO/DeletePathsStep.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using HardCodeLab.RockTomate.Core.Steps;
+using HardCodeLab.RockTomate.Core.Helpers;
+using HardCodeLab.RockTomate.Core.Attributes;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Delete Paths", "Deletes the set of files or directories", StepCategories.FileSystemCategory)]
+    public class DeletePathsStep : SimpleStep
+    {
+        [InputField(tooltip: "Paths which will be removed.", required: true)]
+        public string[] Paths;
+
+        /// <inheritdoc />
+        protected override bool OnStepStart()
+        {
+            foreach (var path in Paths)
+            {
+                if (Directory.Exists(path))
+                {
+                    PathHelpers.DeleteDirectory(path);
+                }
+                else if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+
+            return true;
+        }
+    }
+}
