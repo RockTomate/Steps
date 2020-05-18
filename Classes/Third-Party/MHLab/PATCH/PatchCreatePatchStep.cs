286ff221d81f550d8f75a5a3ccbdc8766eb8de20
Elmar Talibzade <elmar.talibzade@gmail.com>
2020-05-18 16:24:54 +0100

    Added basic steps for third-party assets

---
+++ Classes/Third-Party/MHLab/PATCH/PatchCreatePatchStep.cs
@@ -0,0 +1,32 @@
+﻿#if NET_STANDARD_2_0 || NET_4_6
+
+using System;
+using System.Collections;
+using HardCodeLab.RockTomate.Core.Data;
+using HardCodeLab.RockTomate.Core.Steps;
+using HardCodeLab.RockTomate.Core.Attributes;
+using MHLab.Patch.Core.Admin;
+using MHLab.Patch.Core.Admin.Progresses;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Create Patch", "Creates a patch", "\"PATCH - Updating System\" Plugin")]
+    public class PatchCreatePatchStep : Step
+    {
+        /// <inheritdoc />
+        protected override IEnumerator OnExecute(JobContext context)
+        {
+            var adminBuildContext = new AdminBuildContext(new AdminSettings(), new Progress<BuilderProgress>(Handler));
+            adminBuildContext.BuildVersion
+            var buildBuilder = new BuildBuilder();
+            yield break;
+        }
+
+        private void Handler(BuilderProgress obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+#endif
---
+++ Classes/Third-Party/SIDGIN/SgPatcher/SgPatcherCreatePatchStep.cs
@@ -0,0 +1,55 @@
+﻿#if NET_STANDARD_2_0 || NET_4_6
+
+using System.Collections;
+using Google.Apis.Util;
+using HardCodeLab.RockTomate.Core.Data;
+using HardCodeLab.RockTomate.Core.Steps;
+using HardCodeLab.RockTomate.Core.Attributes;
+using HardCodeLab.RockTomate.Core.Enums;
+using SIDGIN.Patcher.Unity.Editors;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Create Patch", "Creates a patch", "\"SG Patcher\" Plugin")]
+    public class SgPatcherCreatePatchStep : Step
+    {
+        [InputField(tooltip: "Whether a build should be invoked before creating a patch.")]
+        public bool RunBuild = false;
+        
+        private SGPatcherUnityApi _sgPatcherApi = new SGPatcherUnityApi();
+
+        private float _currentProgress = 0f;
+
+        /// <inheritdoc />
+        protected override void OnReset()
+        {
+            _currentProgress = 0f;
+            base.OnReset();
+        }
+
+        /// <inheritdoc />
+        protected override void OnInterrupt()
+        {
+            base.OnInterrupt();
+        }
+
+        /// <inheritdoc />
+        protected override IEnumerator OnExecute(JobContext context)
+        {
+            _sgPatcherApi.onProgressChanged += ProgressChanged;
+            _sgPatcherApi.Patch();
+
+            while (_currentProgress < 1f)
+                yield return null;
+
+            IsSuccess = true;
+        }
+
+        private void ProgressChanged(float arg1, string arg2)
+        {
+            _currentProgress = arg1;
+        }
+    }
+}
+
+#endif
