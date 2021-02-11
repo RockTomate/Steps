44945c63820bfc2bf4bdb6923f530f31bd3b052a
Elmar Talibzade <accounts@hardcodelab.com>
2021-02-11 23:20:10 +0000

    New git step

---
+++ Classes/Git/GitGetBranchesStep.cs
@@ -0,0 +1,44 @@
+﻿#if NET_4_6 || NET_STANDARD_2_0
+
+using System.Collections.Generic;
+using HardCodeLab.RockTomate.Core.Attributes;
+using LibGit2Sharp;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Git - Get Branches", "Retrieves all branches of target repository", StepCategories.GitCategory)]
+    public class GitGetAllBranchesStep : BaseGitStep
+    {
+        [OutputField(tooltip: "Name of a currently checked out branch.")]
+        public string CurrentBranch;
+
+        [OutputField(tooltip: "Array of all branch names.")]
+        public List<string> AllBranches = new List<string>();
+
+        [OutputField(tooltip: "Array of remote branch names.")]
+        public List<string> RemoteBranches = new List<string>();
+
+        protected override bool OnStepStart()
+        {
+            using (var repo = GetRepository())
+            {
+                foreach (var branch in repo.Branches)
+                {
+                    var branchName = branch.FriendlyName;
+
+                    AllBranches.Add(branchName);
+
+                    if (branch.IsRemote)
+                        RemoteBranches.Add(branchName);
+
+                    if (branch.IsCurrentRepositoryHead)
+                        CurrentBranch = branchName;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
+#endif
