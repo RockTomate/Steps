2eaa72b2d636491f078110c9e2dbafe94db4a0df
Elmar Talibzade <accounts@hardcodelab.com>
2021-02-14 18:27:58 +0000

    Added step

---
+++ Classes/Git/GitCommitStep.cs
@@ -0,0 +1,29 @@
+﻿#if NET_4_6 || NET_STANDARD_2_0
+
+using System;
+using LibGit2Sharp;
+using HardCodeLab.RockTomate.Core.Attributes;
+
+namespace HardCodeLab.RockTomate.Steps
+{
+    [StepDescription("Git - Commit", "Commit staged changes to active branch", StepCategories.GitCategory)]
+    public class GitCommitStep : BaseGitStep
+    {
+        [InputField(tooltip: "Commit message", required: true)]
+        public string Message;
+
+        protected override bool OnStepStart()
+        {
+            using (var repo = GetRepository())
+            {
+                var timeOffset = DateTimeOffset.UtcNow;
+                var signature = repo.Config.BuildSignature(timeOffset);
+                repo.Commit(Message, signature, signature);
+            }
+
+            return true;
+        }
+    }
+}
+
+#endif
