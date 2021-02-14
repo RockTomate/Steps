26f1d5818d40df8e01c4016a96a89f65baac1ac2
Elmar Talibzade <accounts@hardcodelab.com>
2021-02-14 03:33:24 +0000

    Minor directory restructuring

--- Classes/Git/Base/BaseGitStep.cs
+++ Classes/Git/Base/BaseGitStep.cs
@@ -9,7 +9,9 @@ namespace HardCodeLab.RockTomate.Steps
 {
     public abstract class BaseGitStep : SimpleStep
     {
-        [InputField(required: true, tooltip: "Directory path where target repository is located.", category: "Config")]
+        protected const string ConfigCategory = "Config";
+
+        [InputField(required: true, tooltip: "Directory path where target repository is located.", category: ConfigCategory)]
         public string RepositoryPath;
 
         protected override bool OnValidate()
