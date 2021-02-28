1dfec98c02fc162c05371af49ef26e6a41d4b048
Elmar Talibzade <accounts@hardcodelab.com>
2021-02-28 19:39:50 +0000

    Now uses LibGit2Sharp's "Commit" instance as a return value instead of a struct as memory crash has been fixed

--- Classes/Git/GitGetBranchHistoryStep.cs
+++ Classes/Git/GitGetBranchHistoryStep.cs
@@ -1,6 +1,5 @@
 ﻿#if NET_4_6 || NET_STANDARD_2_0
 
-using System;
 using System.Linq;
 using System.Collections.Generic;
 using LibGit2Sharp;
@@ -19,47 +18,15 @@ namespace HardCodeLab.RockTomate.Steps
         public int CommitCount = 10;
 
         [OutputField(tooltip: "A list of entire commit history")]
-        public IEnumerable<VCCommit> CommitHistory;
+        public IEnumerable<Commit> CommitHistory;
 
         protected override bool OnStepStart()
         {
-            var commits = CommitCount > 0
+            CommitHistory = CommitCount > 0
                 ? Repository.Commits.Take(CommitCount)
                 : Repository.Commits;
 
-            CommitHistory = commits.Select(VCCommit.Create);
-
             return true;
-        }
-
-        /// <summary>
-        /// Stores light data on a commit.
-        /// Reason why we don't use <see cref="Commit"/> instance directly is because most data in there is lazy-loaded.
-        /// By the time we would need to access its data, the <see cref="Repository"/> instance would already be disposed of, leading to crashes.
-        /// </summary>
-        [Serializable]
-        public struct VCCommit
-        {
-            /// <summary>
-            /// Returns an instance of a VC Commit.
-            /// </summary>
-            /// <param name="commit">Commit instance</param>
-            /// <returns><see cref="VCCommit"/> instance.</returns>
-            public static VCCommit Create(Commit commit)
-            {
-                return new VCCommit
-                {
-                    Author = commit.Author,
-                    Committer = commit.Committer,
-                    Sha = commit.Sha,
-                    Message = commit.Message,
-                };
-            }
-
-            public string Sha { get; private set; }
-            public string Message { get; private set; }
-            public Signature Author { get; set; }
-            public Signature Committer { get; private set; }
         }
     }
 }
