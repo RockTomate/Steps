3187f736c33917ec8d06993ebc9b8e78c12e0b70
Elmar Talibzade <accounts@hardcodelab.com>
2021-05-21 23:39:32 +0100

    Fixed a bug when formulas wouldn't be correctly resolved when overriding job variables

--- Classes/General/RunJobStep.cs
+++ Classes/General/RunJobStep.cs
@@ -76,8 +76,12 @@ namespace HardCodeLab.RockTomate.Steps
             {
                 var customVariable = keyValuePair.Value;
                 var jobVariable = _childSession.RootContext[keyValuePair.Key];
+                var overriddenValue = customVariable.GetValue(context);
                 jobVariable.UseFormula = false;
-                jobVariable.SetValue(customVariable.GetValue());
+                jobVariable.SetValue(overriddenValue);
+
+                RockLog.WriteLine(LogTier.Info, string.Format("Overridden variable named \"{0}\". New value: \"{1}\"",
+                    jobVariable.Name, overriddenValue));
             }
 
             // add new variables
